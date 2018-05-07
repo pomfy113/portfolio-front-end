209009745ee7da7619d1e6c5599e58e8406a396b
pomfy113 <pomfy113@gmail.com>
2018-05-07 10:06:32 -0700

    added to json file; minor adjustment

--- front-end/src/Components/Jumbotron/Project/text.js
+++ front-end/src/Components/Jumbotron/Project/text.js
@@ -1,15 +1,20 @@
 const data = [
     {
         title: 'Our Anime Chart',
-        subtitle: 'Single-Page Application for Seasonal Anime Browsing'
+        subtitle: 'Single-Page Application for Seasonal Anime Browsing',
+        liveSite: 'http://ouranimechart.herokuapp.com/'
     },
     {
         title: 'Sandpiper',
-        subtitle: 'Playlist Migration Application'
+        subtitle: 'Playlist Migration Application',
+        liveSite: 'http://www.sandpiper.ninja/'
+
     },
     {
         title: '3d20',
-        subtitle: '3D Tabletop Board Generator'
+        subtitle: '3D Tabletop Board Generator',
+        liveSite: 'https://board-3d20.herokuapp.com/'
+
     }
 ]
 
