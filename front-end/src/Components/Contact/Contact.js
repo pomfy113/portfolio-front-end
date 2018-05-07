1ee53c7a82294d047c74b3075da3b2ea8d3b527b
pomfy113 <pomfy113@gmail.com>
2018-05-07 13:44:03 -0700

    Contacts fine tuning

--- front-end/src/Components/Contact/Contact.js
+++ front-end/src/Components/Contact/Contact.js
@@ -6,10 +6,24 @@ export default class Contact extends Component {
     return (
       <div id="Contact">
           <h1>Ferdinand Cruz</h1>
-          <p>San Francisco, CA, (925) 395-6845</p>
-          <p><b>Email:</b> cruz.ferdin@gmail.com</p>
-          <p><b>Github:</b> github.com/pomfy113</p>
-          <p><b>LinkedIn:</b> /in/ferdinand-cruz113/</p>
+          <h2>Full-Stack Web Developer</h2>
+          <div id='contact-container'>
+              <div id='contact-text'>
+                  <p>San Francisco, CA</p>
+                  <p>(925) 395-6845</p>
+              </div>
+              <div id='contact-links'>
+                <a href='https://github.com/pomfy113' target='_about'>
+                    <img src='contact/github.png'/>
+                </a>
+                <a href='https://www.linkedin.com/in/ferdinand-cruz113/' target='_about'>
+                    <img src='contact/LinkedIn.png'/>
+                </a>
+                <a href='mailto:cruz.ferdin@gmail.com'>
+                    <img src='contact/mail.png'/>
+                </a>
+              </div>
+          </div>
       </div>
     );
   }
