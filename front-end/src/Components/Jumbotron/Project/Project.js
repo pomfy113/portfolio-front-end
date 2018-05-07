9c3c2415b0dbaab380571fef3c6fa1afeafa1102
pomfy113 <pomfy113@gmail.com>
2018-05-07 12:38:22 -0700

    Links and tools up

--- front-end/src/Components/Jumbotron/Project/Project.js
+++ front-end/src/Components/Jumbotron/Project/Project.js
@@ -9,11 +9,13 @@ export default class Project extends Component {
   }
 
   render() {
-      let title, subtitle, tools;
+      let title, subtitle, tools, site, repo;
       if(this.projectData){
           title = this.projectData.title;
           subtitle = this.projectData.subtitle;
-          tools = this.projectData.tools
+          tools = this.projectData.tools;
+          site = this.projectData.liveSite;
+          repo = this.projectData.repo;
       }
 
       return(
@@ -21,6 +23,11 @@ export default class Project extends Component {
               <h1>{title}</h1>
               <h2>{subtitle}</h2>
               <img alt='Project screenshot' src={`projects/project-${this.props.index}/image.png`}/>
+              <div className='url-links'>
+                  <a href={site} target='_about'><button>Site</button></a>
+                  <a href={repo} target='_about'><button>Repo</button></a>
+              </div>
+
               <p>Tools:<br/>{tools}</p>
           </div>
       )
--- front-end/src/Components/Jumbotron/Project/text.js
+++ front-end/src/Components/Jumbotron/Project/text.js
@@ -3,21 +3,26 @@ const data = [
         title: 'Our Anime Chart',
         subtitle: 'Single-Page Application for Seasonal Anime Browsing',
         liveSite: 'http://ouranimechart.herokuapp.com/',
+        repo: 'https://github.com/pomfy113/Anime-Ratings',
         tools: `v1 - Basic Site - Node.js & Express, Handlebars, MongoDB, Bootstrap
-        v2 - Single-Page - Node.js & Express, Handlebars, jQuery
-        v3 - React & UX Testing - Node.js & Express, React, Redux (WIP)`
+        v2 - Single-Page - Node.js & Express, Handlebars, jQuery, CSS
+        v3 - React & UX Testing - Node.js & Express, React, Redux (WIP), CSS`
     },
     {
         title: 'Sandpiper',
         subtitle: 'Playlist Migration Application',
-        liveSite: 'http://www.sandpiper.ninja/'
+        liveSite: 'http://www.sandpiper.ninja/',
+        repo: 'https://github.com/sgalizia/SandPiper',
+        tools: `Front-End - React, Redux, Node & Express, CSS`
 
     },
     {
         title: '3d20',
         subtitle: '3D Tabletop Board Generator',
-        liveSite: 'https://board-3d20.herokuapp.com/'
+        liveSite: 'https://board-3d20.herokuapp.com/',
+        repo: 'https://github.com/pomfy113/Board-Project',
 
+        tools: `Native Javascript and CSS, Node & Express`
     }
 ]
 
